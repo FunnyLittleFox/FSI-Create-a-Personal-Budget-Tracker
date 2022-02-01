2ccbb43b5cc5c6ee34115e8851cda77cf1c698d5
Fox Flaherty <katyra14@gmail.com>
2022-01-31 19:25:51 -0800

    Step 3 Start

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+window.prompt(weeklyExpenseQuetions[0])
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+let stringAnswer = winder.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
